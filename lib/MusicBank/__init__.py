6e4589daed4b816b341b9a34c7fc19442f971c4a
Josef Grosch <jgrosch@gmail.com>
2023-12-18 12:26:27 -0800

    Added lib skeleton

---
+++ lib/MusicBank/__init__.py
@@ -0,0 +1,98 @@
+# -*- mode: python -*-
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------------------
+#
+#                             < __init__.py >
+#
+# -----------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------
+#
+# File Name    : __init__.py
+#
+# Author       : Josef Grosch
+#
+# Date         : 28 Jul 2023
+#
+# Modification : Some
+#
+# Application  :
+#
+# Description  :
+#
+# Notes        :
+#
+# Functions    :
+#
+# -----------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------
+#
+#                              Copyright
+#
+#                   (C) Copyright 2023 Moose River LLC.
+#                           <jgrosch@gmail.com>
+#
+#                         All Rights Reserved
+#
+#               Deadicated to my brother Jerry Garcia,
+#            who passed from this life on August 9, 1995.
+#                     Happy trails to you, Jerry
+#
+# -----------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------
+#
+#                               GPG Key
+#
+# pub   1024D/2D466121 2003-04-08
+# Key   fingerprint = 1D0C 4B35 9A42 2A05 39CE  7F54 CBCA 6B2D 2D46 6121
+# uid   Josef Grosch (Berkeley, Ca.) <jgrosch@mooseriver.com>
+#
+# -----------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------
+#
+#                         Contact Information
+#
+
+#                          Moose River LLC.
+#                            P.O. Box 9403
+#                         Berkeley, Ca. 94709
+#
+#                      http://www.mooseriver.com
+#
+# -----------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------
+#
+# Import
+#
+# -----------------------------------------------------------------------
+
+
+"""
+MooseRiver
+
+The main package for the MooseRiver code base.
+
+"""
+
+# -- start constants --
+# @depends: python (>=3.7)
+
+__author__      = 'Josef Grosch <jgrosch@gmail.com'
+__copyright__   = 'Copyright (c) 2023 MooseRiver, LLC.'
+__description__ = ''
+__revision__    = '0.2'
+__version__     = '0.2'
+
+# -- end constants --
+
+
